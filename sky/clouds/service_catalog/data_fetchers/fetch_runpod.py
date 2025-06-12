4e26cd02631b69fc1527a405d63e5ad5de31d172
Alon Kellner <me@alonkellner.com>
2025-06-12 16:44:54 +0300

    fix: correct regions and zones

--- sky/clouds/service_catalog/data_fetchers/fetch_runpod.py
+++ sky/clouds/service_catalog/data_fetchers/fetch_runpod.py
@@ -11,24 +11,46 @@ from sky.provision.runpod.utils import GPU_NAME_MAP
 from sky.clouds.service_catalog.constants import CATALOG_DIR, CATALOG_SCHEMA_VERSION
 
 
-REGIONS = [
-    "AE",
-    "AR",
-    "AT",
-    "BG",
-    "CA",
-    "CZ",
-    "ES",
-    "FR",
-    "IL",
-    "NL",
-    "PT",
-    "SE",
-    "SK",
-    "TT",
-    "US",
-    "ZA",
-]
+ZONES_TO_REGIONS = {
+    "OC-AU-1": "AU",
+    "CA-MTL-1": "CA",
+    "CA-MTL-2": "CA",
+    "CA-MTL-3": "CA",
+    "CA-MTL-4": "CA",
+    "EU-CZ-1": "CZ",
+    "EU-FR-1": "FR",
+    "AP-JP-1": "JP",
+    "EU-NL-1": "NL",
+    "EU-RO-1": "RO",
+    "EU-SE-1": "SE",
+    "EU-SE-2": "SE",
+    "EUR-NO-1": "NO",
+    "EUR-IS-1": "IS",
+    "EUR-IS-2": "IS",
+    "EUR-IS-3": "IS",
+    "SEA-SG-1": "SG",
+    "US-CA-1": "US",
+    "US-CA-2": "US",
+    "US-DE-1": "US",
+    "US-GA-1": "US",
+    "US-GA-2": "US",
+    "US-IL-1": "US",
+    "US-KS-1": "US",
+    "US-KS-2": "US",
+    "US-KS-3": "US",
+    "US-NC-1": "US",
+    "US-TX-1": "US",
+    "US-TX-2": "US",
+    "US-TX-3": "US",
+    "US-TX-4": "US",
+    "US-TX-5": "US",
+    "US-WA-1": "US",
+    "US-OR-1": "US",
+    "US-OR-2": "US",
+    "US-MO-1": "US",
+}
+
+
 ENDPOINT = "https://api.runpod.io/graphql"
 
 
@@ -38,44 +60,49 @@ def get_raw_runpod_prices(is_secure: bool) -> pd.DataFrame:
         {
             "query": f"""
 query {{
-    gpuTypes {{
-        id
-        displayName
-        memoryInGb
-        maxGpuCount
-        lowestPrice(input: {{
-            gpuCount: 1,
-            minDisk: 0,
-            minMemoryInGb: 8,
-            minVcpuCount: 2,
-            secureCloud: {"true" if is_secure else "false"},
-            compliance: null,
-            dataCenterId: null,
-            globalNetwork: false
-        }}) {{
-            minimumBidPrice
-            uninterruptablePrice
-            minVcpu
-            minMemory
-            stockStatus
-            compliance
-        }}
-        nodeGroupDatacenters {{
-          id
-          name
-          location
-          globalNetwork
-          storageSupport
-          listed
-        }}
-        securePrice
-        communityPrice
-        oneMonthPrice
-        oneWeekPrice
-        threeMonthPrice
-        sixMonthPrice
-        secureSpotPrice
+  gpuTypes {{
+    id
+    displayName
+    memoryInGb
+    maxGpuCount
+    securePrice
+    communityPrice
+    oneMonthPrice
+    oneWeekPrice
+    threeMonthPrice
+    sixMonthPrice
+    secureSpotPrice
+    lowestPrice(input: {{
+        gpuCount: 1,
+        minDisk: 0,
+        minMemoryInGb: 8,
+        minVcpuCount: 2,
+        secureCloud: {"true" if is_secure else "false"},
+        compliance: null,
+        dataCenterId: null,
+        globalNetwork: false
+    }}) {{
+        minimumBidPrice
+        uninterruptablePrice
+        minVcpu
+        minMemory
+        stockStatus
+        compliance
     }}
+  }}
+  dataCenters {{
+    id
+    name
+    location
+    gpuAvailability {{
+      available
+      stockStatus
+      gpuTypeId
+      gpuTypeDisplayName
+      displayName
+      id
+    }}
+  }}
 }}
 """
         }
@@ -85,16 +112,45 @@ query {{
 
     response = requests.post(ENDPOINT, headers=headers, data=payload)
     response_data = response.json()
-    # return response_data
-    # Create a dataframe with GPU metadata for all types
-    df = pd.DataFrame(response_data["data"]["gpuTypes"])
+    df = pd.DataFrame(response_data["data"]["dataCenters"])
+    df = df.explode("gpuAvailability").reset_index(drop=True)
     df = pd.concat(
         [
-            df.drop("lowestPrice", axis=1),
-            pd.json_normalize(df["lowestPrice"]).add_prefix("lowestPrice."),
+            df.drop("gpuAvailability", axis=1),
+            pd.json_normalize(df["gpuAvailability"]).add_prefix("gpuAvailability."),
         ],
         axis=1,
     )
+
+    api_zones = set(df["name"].unique())
+    expected_zones = set(ZONES_TO_REGIONS.keys())
+
+    missing_zones = api_zones - expected_zones
+    if len(missing_zones) > 0:
+        print(
+            f"WARNING! Some data center names in the RunPod API are not mapped to regions: {missing_zones}. "
+        )
+    extra_zones = expected_zones - api_zones
+    if len(extra_zones) > 0:
+        print(
+            f"WARNING! Some data center names in ZONES_TO_REGIONS are not present in the RunPod API: {extra_zones}. "
+        )
+
+    df["region"] = df["name"].map(ZONES_TO_REGIONS)
+    df["gpu_id"] = df["gpuAvailability.id"]
+
+    gpu_df = pd.DataFrame(response_data["data"]["gpuTypes"])
+    gpu_df = pd.concat(
+        [
+            gpu_df.drop("lowestPrice", axis=1),
+            pd.json_normalize(gpu_df["lowestPrice"]).add_prefix("lowestPrice."),
+        ],
+        axis=1,
+    )
+    gpu_df["gpu_id"] = gpu_df["id"]
+
+    df = df.join(gpu_df.set_index("gpu_id"), on="gpu_id", rsuffix=".gpu")
+
     return df
 
 
@@ -108,6 +164,8 @@ def get_partial_runpod_catalog(is_secure: bool) -> pd.DataFrame:
             "lowestPrice.minimumBidPrice": "SpotPrice",
             "lowestPrice.minVcpu": "vCPUs",
             "lowestPrice.minMemory": "MemoryGiB",
+            "region": "Region",
+            "name": "AvailabilityZone",
         }
     )
 
@@ -119,11 +177,11 @@ def get_partial_runpod_catalog(is_secure: bool) -> pd.DataFrame:
     extra_ids = mapping_ids - runpod_ids
     if len(missing_ids) > 0:
         print(
-            f"WARNING! Some machine ids from runpod api were missing from runpod mapping: {missing_ids}"
+            f"WARNING! Some machine ids from RunPod API were missing from GPU_NAME_MAP: {missing_ids}"
         )
     if len(extra_ids) > 0:
         print(
-            f"WARNING! Some machine ids in runpod mapping do not exist in runpod api: {extra_ids}"
+            f"WARNING! Some machine ids in GPU_NAME_MAP do not exist in RunPod API: {extra_ids}"
         )
     runpod["AcceleratorName"] = runpod["id"].replace(REVERSE_GPU_MAP)
 
@@ -160,10 +218,6 @@ def get_partial_runpod_catalog(is_secure: bool) -> pd.DataFrame:
     for c in ["Price", "SpotPrice", "vCPUs", "MemoryGiB"]:
         runpod_exploded[c] = runpod_exploded[c] * runpod_exploded["AcceleratorCount"]
 
-    # Map region names to their codes
-    runpod_exploded["Region"] = runpod_exploded["id"].apply(lambda x: REGIONS)
-
-    runpod_exploded = runpod_exploded.explode("Region").reset_index(drop=True)
     # Filter & Reorder dataframe columns to match the catalog scheme
     formatted_runpod = runpod_exploded[
         [
@@ -176,6 +230,7 @@ def get_partial_runpod_catalog(is_secure: bool) -> pd.DataFrame:
             "Region",
             "SpotPrice",
             "Price",
+            "AvailabilityZone",
         ]
     ]
     return formatted_runpod
