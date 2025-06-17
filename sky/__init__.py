1208f76f0d27397ff6d39af02938b080bcacf11c
Alon Kellner <me@alonkellner.com>
2025-06-17 14:34:01 +0300

    fix: custom semver version

--- sky/__init__.py
+++ sky/__init__.py
@@ -35,7 +35,7 @@ def _get_git_commit():
 
 
 __commit__ = _get_git_commit()
-__version__ = '0.9.3'
+__version__ = '0.9.3+alonkellner.0'
 __root_dir__ = os.path.dirname(os.path.abspath(__file__))
 
 
